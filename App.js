beb10079a47e5664c36ad6d814e1ea9a968a7085
vijay kanaujia <vijaykanaujia3@gmail.com>
2023-03-31 08:29:55 +0530

    episode 1 code

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("div", {}, [
+  React.createElement("h2", {}, "Hi i am from React"),
+  React.createElement("div", { class: "card" }, [
+    React.createElement("div", { class: "card-header" }, [
+      React.createElement("h3", { class: "title" }, "Title"),
+      React.createElement("h6", { class: "subtitle" }, "Subitle"),
+    ]),
+    React.createElement("div", { class: "card-body" }, 'Content'),
+    React.createElement("div", { class: "card-footer" }, 'Footer'),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
