2e09bc308ac56920ec80a8ad76b159f5af1afd90
vijay kanaujia <vijaykanaujia3@gmail.com>
2023-04-03 00:47:15 +0530

    episode 4 food ordering

--- App.js
+++ App.js
@@ -1,30 +1,94 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span>Reaact Element + </span>;
-const Title = () => (
-  <h1 className="">
-    {elem}
-    Namaste React Using JSX
-  </h1>
-);
-
-//component composition
-const HeadingComponent = () => {
+const HeaderComponent = () => {
   return (
-    // <React.Fragment></React.Fragment>
-    <>
-      <div id="container">
-        {Title()}
-        <Title />
-        <Title></Title>
-        <h1 className="heading">
-          Namaste React function component {100 + 200}
-        </h1>
+    <div id="header">
+      <div className="logo-container">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbSU8FA12DS4IhTb9vsaB1tH1QcaAyS_X_YA&usqp=CAU" />
       </div>
-      <div className="container-2">React fregment</div>
-    </>
+      <div id="menu_container">
+        <ul className="menu">
+          <li className="menu-item">Home</li>
+          <li className="menu-item">Item</li>
+          <li className="menu-item">Contact Us</li>
+          <li className="menu-item">Offer</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const FooterComponent = () => {
+  return <div id="footer">&copy; ThoughtSole</div>;
+};
+
+const RestaurantCard = (props) => {
+  console.log(props);
+  const { name, thumbnailUrl, rating, cuisines, costForTwo, deliveryTime } =
+    props.restaurents;
+  return (
+    <div className="res-card">
+      <img src={thumbnailUrl} />
+      <h3>{name}</h3>
+      <p>⭐ {rating}</p>
+      <p>{cuisines.join(", ")}</p>
+      <p>{costForTwo / 100} cost for Two</p>
+      <p>delivery in {deliveryTime} min</p>
+    </div>
+  );
+};
+
+const resturentList = [
+  {
+    id: 1,
+    name: "KFC",
+    thumbnailUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hbeotimtlfwmlg68iuca",
+    rating: 3.4,
+    cuisines: ["Burgers", "American"],
+    costForTwo: 40000,
+    deliveryTime: 20,
+  },
+  {
+    id: 2,
+    name: "KFC",
+    thumbnailUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/hbeotimtlfwmlg68iuca",
+    rating: 3.4,
+    cuisines: ["Burgers", "American"],
+    costForTwo: 40000,
+    deliveryTime: 20,
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div id="search_bar">
+        <input type="search" placeholder="Search Restaurent" name="searchbar" />
+      </div>
+      <div id="restaurent_card_container">
+        {resturentList.map((restaurents) => {
+          return <RestaurantCard key={restaurents.id} restaurents={restaurents} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="container">
+      <header>
+        <HeaderComponent />
+      </header>
+      <Body />
+      <footer>
+        <FooterComponent />
+      </footer>
+    </div>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
